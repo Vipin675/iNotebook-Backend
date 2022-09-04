2a3fab3f2eda7a106ac54ff6946b234c2b56c026
Vipin <vipinvishwa1m1@gmail.com>
2022-09-04 15:26:56 +0530

    initialized server with mongoose

---
+++ db.js
@@ -0,0 +1,16 @@
+// getting-started.js
+const mongoose = require("mongoose");
+
+const mongooseURI = "mongodb://localhost:27017/";
+
+main()
+  .catch((err) => console.log(err))
+  .then(() => {
+    console.log("Connected to the datbase successfully");
+  });
+
+async function main() {
+  await mongoose.connect(`${mongooseURI}`);
+}
+
+module.exports = main;
