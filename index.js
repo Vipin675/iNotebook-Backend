1d4ff2019ea84d28d07cd06fe07b7df04b12ef02
Vipin <vipinvishwa1m1@gmail.com>
2022-09-08 21:19:21 +0530

    installed cors package to solve cors errors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const connectToMongo = require("./db");
+const cors = require("cors");
 
 connectToMongo();
 const app = express();
+app.use(cors());
 const port = 5000;
 
 app.use(express.json());
